381df9d5fb6e68fef67bfe9eda57b684e5939b30
Jibrin Baba Isah <38309101+jibrinbabaisah@users.noreply.github.com>
2019-05-19 09:32:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,362 @@
+<?php 
+session_start();
+include 'includes/init.php';
+ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html class="" idmmzcc-ext-docid="363114496" xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="shortcut icon" href="favicon.ico">
+<link href="Resources/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="Resources/css/dpps.css" rel="stylesheet">
+<link href="Resources/css/alumni.css" rel="stylesheet">
+<link href="Resources/font-awesome/css/font-awesome.css" rel="stylesheet">
+<link href="Resources/css/datepicker.css" rel="stylesheet">
+<link href="Resources/css/jasny-bootstrap.css" rel="stylesheet">
+<link href="Resources/css/tabcontent.css" rel="stylesheet">
+
+<script src="Resources/js/jquery-1.js"></script>
+<script src="Resources/js/bootstrap.js"></script>
+<script type="text/javascript" src="Resources/js/jquery-lib.htm"></script>
+<script src="Resources/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+<link href="Resources/css/jquery.css" rel="stylesheet">
+<script src="Resources/css/jquery_004.js"></script>
+<link href="Resources/css/jquery.css" type="text/css" rel="stylesheet">  
+<script src="Resources/js/jquery.htm"></script>
+<script src="Resources/js/jquery.js"></script>
+<script src="Resources/js/tabcontent.js" type="text/javascript"></script>
+
+<link rel="stylesheet" href="Resources/css/style-newsticker.css" type="text/css" media="screen">
+<script src="Resources/js/jcarousellite_1.js" type="text/javascript"></script>
+
+
+<link rel="stylesheet" href="Resources/css/bootstrap-theme.css">
+<link href="Resources/css/site.css" rel="stylesheet" type="text/css">
+<script src="Resources/js/jquery_003.js" type="text/javascript"></script>
+
+
+ <link href="Resources/css/simpleSlider_002.css" rel="stylesheet" type="text/css" media="screen">   
+    <link href="Resources/css/simpleSlider.css" rel="stylesheet" type="text/css" media="screen">
+    <!-- /IMPORTANT : must before the javascripts -->
+
+	
+	<script type="text/javascript" src="Resources/js/jquery_002.js"></script>
+	<script type="text/javascript" src="Resources/js/jquery_005.js"></script>
+  
+<title>UDUTH</title>
+<script type="text/javascript">
+		$(document).ready(function () {
+			$('#slider').simpleSlider({
+			    width: 600,
+			    height: 300
+			});
+		});
+	</script>
+<style type="text/css">.fancybox-margin{margin-right:17px;}</style></head>
+
+<body class="bodyy">
+<div class="container">
+<div style="margin-left:165px;"><img src="Resources/Images//banner.png" height="130" width="800" align="middle"></div>
+<div class="content large_radius">
+     <div class="bordex">
+     
+     <div class="col col-md-8">
+     <!--<div class="imge"><img src="images/simg.png" width="" height="" style="margin-left:10px; margin-top:10px;" /></div>-->
+     <div class="simpleSlider theme-mordilion" id="simpleSlider-slider"><div style="height: 300px; width: 600px; overflow: hidden; position: relative;" id="slider" class="">
+        <ul>
+	        <li style="position: absolute; width: 600px; height: 300px; z-index: 9110;">
+	            <img src="Resources/Images/UDUTH.JPG" alt="Borderlands" height="300" width="600">
+	            <div style="display: none;">
+	                <h3 style="margin-top: 0;">UDUTH MAIN ENTRANCE GATE</h3>
+	                
+	                <a href="#" target="_blank">read more...</a>
+	            </div>
+	        </li>
+           
+	        <li>
+	            <img src="Resources/Images/vaccine.JPG" width="600" height="300"  alt="Deus Ex 3 - Human Revolution" />
+	            <div>
+	                <h3 style="margin-top: 0;">Getting your kids vaccinated is the best way to protect them from life-threatening diseases, such as polio and measles.</h3>
+	                <a href="#" target="_blank">website</a>
+	            </div>
+	        </li>
+	        <li>
+	            <img src="Resources/Images/hibbah.png" width="600" height="300"  alt="Portal 2" />
+	            <div>
+	                <h3 style="margin-top: 0;">Generate A Birth Certificate And Give Your Baby A Legal Recognition</h3>
+	                <a href="#" target="_blank">website</a>
+	            </div>
+	        </li>
+
+
+	        <li>
+	            <img src="Resources/Images/vaccination.JPG" width="600" height="300"  alt="Portal 2" />
+	            <div>
+	                <h3 style="margin-top: 0;">Oral Polio Vaccine</h3>
+	                <a href="#" target="_blank">website</a>
+	            </div>
+	        </li>
+	        <li>
+	            <img src="Resources/Images/baby.JPG" width="600" height="300"  alt="Team Fortres 2" />
+	            <div>
+	                <h3 style="margin-top: 0;">Newly Born Baby</h3>
+	                <a href="#" target="_blank">website</a>
+	            </div>
+	        </li>
+        </ul>
+    <div style="bottom: 0px; display: block; left: 0px; width: 580px; position: absolute; z-index: 9120;" class="simpleSlider-title" id="simpleSlider-slider-title"><div style="display: block; height: 100%; left: 0px; opacity: 0.7; position: absolute; top: 0px; width: 100%; z-index: -1;" class="simpleSlider-title-transparency" id="simpleSlider-slider-title-transparency"></div><div class="simpleSlider-title-content" id="simpleSlider-slider-title-content">
+	                <h3 style="margin-top: 0;">UDUTH</h3>
+	                
+	                <a href="#" target="_blank">read more...</a>
+	            </div></div><div class="simpleSlider-navigation" id="simpleSlider-slider-navigation" style="position: absolute; width: 600px; height: 300px; z-index: 9110;"><a style="display: none;" class="simpleSlider-navigation-previous" id="simpleSlider-slider-navigation-previous" href="javascript:void(0);" rel="prev">prev</a><a style="display: none;" class="simpleSlider-navigation-next" id="simpleSlider-slider-navigation-next" href="javascript:void(0);" rel="next">next</a></div></div><ol style="z-index: 9120;" class="simpleSlider-navigation-buttons" id="simpleSlider-slider-navigation-buttons"><li><a class="simpleSlider-navigation-button current" id="simpleSlider-slider-navigation-button-1" href="javascript:void(0);" rel="0"></a></li></ol></div>
+     </div>
+     
+     <div class="col col-md-4">
+     <div class="logg" style="padding-top:10px; border-bottom:0px;">
+     
+             <div style="width:140px; margin:0 auto;"><img src="Resources/Images//login_img.png"></div>
+             <div style="width:200px; margin:0 auto;">
+             <?php 
+             	if (isset($_POST['login'])) {
+             		$username = @mysql_real_escape_string($_POST['username']);
+             		$password = @mysql_real_escape_string($_POST['password']);
+
+             		$query = @mysqli_query($db, "SELECT * FROM admin WHERE username = '$username' AND password = '$password'");
+             		$row = @mysqli_fetch_array($query);
+
+
+             		$query2 = @mysqli_query($db, "SELECT * FROM users WHERE username ='$username' AND password ='$password'");
+             		$row1 = @mysqli_fetch_array($query2);
+
+             		if ($row > 0) {
+             			$_SESSION['admin'] = $username;
+             			$_SESSION['admin_id'] = $row['admin_id'];
+             			 echo "<script>window.location='admin.php'</script>";
+             		}elseif ($row1 > 0) {
+             			$_SESSION['user'] = $username;
+             			$_SESSION['user_id'] = $row1['user_id'];
+             			 echo "<script>window.location='myaccount.php'</script>";
+
+             		}else{
+             			echo "<script>alert('Wrong Username/Password');window.location='index.php';</script>";
+             		}
+             		
+             	}
+
+              ?>
+              <form method="post" id="login_form">
+             <div class="input-group" style="margin-bottom:5px;">
+             <input class="form-control black" placeholder="Username" name="username" id="username" data-toggle="tooltip" type="text">
+             </div>
+             
+             <div class="input-group" style="margin-bottom:5px;">
+             <input class="form-control black" placeholder="Password" name="password" id="password" type="password">
+             </div>
+             
+             <div class="input-group col col-md-2" style="width:90px;">
+             <input class="btn btn-success" name="login" value="Login" type="submit">
+             </div>
+             </form>
+    
+           <div class="input-group col col-md-7">
+           </div>
+
+        </div>
+              <div id="login_msg"></div>
+            <a href="forgot-password.php"> <div class="btn btn-primary" style="width:260px; margin-top:10px;">Click here to reset password</div></a>
+           <div style="margin-bottom:16px; margin-top:0px; height:30px;"> <a href="Registration.php" class="login_div"> <div class="btn btn-primary" style="width:260px; margin-top:10px;">Click here to sign up</div></a></div>
+            
+            
+     </div>
+     </div>
+     <div class="clearfix"></div>
+     </div>
+ 
+ 
+ <div class="inin">
+ <div class="col col-md-12">
+ 
+        
+<div class="row">
+<div class="col-md-12">
+<div class="panel panel-default">
+<div class="panel-heading"> <span class="fa fa-list-alt"></span><b> News/Events</b></div>
+<div class="panel-body">
+<div class="row">
+<div class="col-xs-12">
+<ul style="overflow-y: hidden; height: 120px;" class="demo1">
+
+
+
+
+
+<li style="display: list-item;" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li>
+<li style="display: list-item;" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+
+</tbody></table>
+</li>
+<li style="" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li><li style="" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li><li style="display:none;" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li><li style="display:none;" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li><li style="display:none;" class="news-item">
+<table cellpadding="4">
+<tbody><tr>
+
+<td>New Event...... <a href="#">Read more...</a></td>
+</tr>
+</tbody></table>
+</li></ul>
+</div>
+</div>
+</div>
+<div class="panel-footer"><ul class="pagination pull-right" style="margin: 0px; display: none;"><li><a href="#" class="prev"><span class="fa fa-chevron-down"></span></a></li><li><a href="#" class="next"><span class="fa fa-chevron-up"></span></a></li></ul><div class="clearfix"></div></div>
+</div>
+</div>
+
+</div>
+        
+ </div>
+    <div class="col col-md-3">
+    
+    
+    
+    
+    </div>
+
+<div class="clearfix"></div>
+ </div>
+ 
+</div>
+                        
+          
+                        
+ </div>
+
+
+
+<script>
+ 	$(".login_div").click(function() {
+				$.fancybox.open({
+					href : 'signup.php',
+					type : 'iframe',
+					padding : 5,
+					height:450,
+					width:650,
+				});
+				$.fancybox.resize();
+				$.fancybox.update();
+			});
+			
+			
+$("#regGuide").click(function() {
+				$.fancybox.open({
+					href : 'reg_guide.php',
+					type : 'iframe',
+					padding : 5,
+					height:450,
+					width:650,
+				});
+				$.fancybox.resize();
+				$.fancybox.update();
+			});
+$("#contact").click(function() {
+				$.fancybox.open({
+					href : 'our_contact.php',
+					type : 'iframe',
+					padding : 5,
+					height:450,
+					width:650,
+				});
+				$.fancybox.resize();
+				$.fancybox.update();
+			});						
+			
+function add(){
+	var fd="task=login&"+$('#login_form').serialize();
+	//alert(fd);
+	$.ajax({
+			
+		type:'POST',
+		url: 'processForm.php',
+		data: fd,
+		success: function(data){
+		 $('#login_msg').html(data);		
+		},
+	});
+	}			
+	
+	
+	 $(function () {
+        $(".demo1").bootstrapNews({
+            newsPerPage: 4,
+            autoplay: true,
+			pauseOnHover:true,
+            direction: 'up',
+            newsTickerInterval: 4000,
+            onToDo: function () {
+                //console.log(this);
+            }
+        });
+		
+		
+
+        $("#demo3").bootstrapNews({
+            newsPerPage: 3,
+            autoplay: false,
+            
+            onToDo: function () {
+                //console.log(this);
+            }
+        });
+    });
+	
+	function tester(){
+	
+	      BootstrapDialog.confirm('Hi Apple, are you sure?', function(result){
+            if(result) {
+                alert('Yup.');
+            }else {
+                alert('Nope.');
+            }
+        });
+	
+	}
+ </script>
+</body></html>
